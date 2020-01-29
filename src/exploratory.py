c1997e0cb48d352338e994ef5c5b569d02d4f412
luanps <luann.porfirio@gmail.com>
2020-01-29 20:17:31 -0300

    basic layout

---
+++ src/exploratory.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+class ExploratoryAnalysis:
+
+    def __init__():
+        pass
---
+++ src/preproc.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+class Preproc:
+
+    def __init__():
+        pass
