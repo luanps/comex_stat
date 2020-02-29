263b842807270e826a7c4204e3f78266cb622602
luanps <luann.porfirio@gmail.com>
2020-02-28 22:45:45 -0300

    update data exploration: check zeros on numerical data, read the correct data from file, drop unnecessary columns

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from sklearn.linear_model import LogisticRegression
-
+import pandas as pd
+import pdb
 from src.utils import load_dataset
 from src.exploratory import ExploratoryAnalysis
 from src.preproc import Preproc
 from src.model import Model
+pd.set_option('display.max_rows', 1000)
 
 def data_exploration(data):
     exploratory = ExploratoryAnalysis(data)
@@ -13,33 +15,38 @@ def data_exploration(data):
         samples with {exploratory.data_length()[1]} attributes.\n""")
     print(f"""These attributes are:\n {exploratory.list_attributes()}""")
     print(f"""Attributes with null data:\n {exploratory.check_null()}""")
-
     empty_spaces = exploratory.check_empty_spaces()
     print(f"""Textual attributes with empty data (value=' '):\n{empty_spaces}""")
+    zeros = exploratory.check_zeros()
+    print(f"""Numerical attributes with zeros:\n{zeros}""")
 
     unique_values = exploratory.check_unique_values(10)
     print(f"""Sample of each attribute:""")
     [print(key, value) for key, value in unique_values.items()]
-    return empty_spaces
+    return empty_spaces, zeros
 
 
 if __name__ == '__main__':
     print("============= Reading data from file  =============")
-    filepath = 'data/database.csv'
+    filepath = 'data/listings_rio.csv'
     data = load_dataset(filepath)
 
     print("============= Data exploration  =============")
-    empty_spaces = data_exploration(data)
+    empty_spaces, zeros = data_exploration(data)
 
     print("============= Applying Preprocessing step =============")
-    columns_to_drop = ['customerID','code','Hash']
+
+    columns_to_drop = ['id', 'name', 'host_id', 'host_name', 'longitude',
+                       'neighbourhood_group', 'last_review', 'latitude', 
+                       'reviews_per_month', 'calculated_host_listings_count']
+    
     preproc = Preproc(data, columns_to_drop, empty_spaces)
     treated_data = preproc.apply_preproc()
 
     print("============= Data exploration after preprocessing  =============")
     data_exploration(treated_data)
 
-    print("============= Plotting data  =============")
+    '''print("============= Plotting data  =============")
     ExploratoryAnalysis.plot_data(treated_data)
 
     print("============= Encoding data  =============")
@@ -49,4 +56,4 @@ if __name__ == '__main__':
     lr_model = LogisticRegression()
     model_name = 'logistic_regression'
     model = Model(lr_model,model_name)
-    model.run_model(encoded_data)
+    model.run_model(encoded_data)'''
