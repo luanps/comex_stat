convert textual data into number (counting number of words), fix quantile ranges when converting numerical data into category, refactor code.

src/preproc.py
@@ -1,30 +1,29 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
+import re
 import pdb
 
 class Preproc:
 
     def __init__(self, data, columns_to_drop, country, obj_to_float,
-        num_to_categories):
-        self.data = data
+        num_to_categories, text_to_counter):
+        self.data = data.copy()
         self.columns_to_drop = columns_to_drop
         self.country = country
         self.obj_to_float = obj_to_float
         self.num_to_categories = num_to_categories
+        self.text_to_counter = text_to_counter
 
     def drop_columns(self):
         self.data.drop(self.columns_to_drop,axis=1, inplace=True)
 
 
     def clean_signs(self, data_item):
-        cleaned_dolar = data_item.str.replace('$','')
+        lower_text = data_item.str.lower()
+        cleaned_dolar = lower_text.str.replace('$','')
         cleaned_percent = cleaned_dolar.str.replace('%','')
         return cleaned_percent
-
-
-    def normalize_text(self, data_item):
-        return data_item.str.lower()
 
     
     def map_binary_text(self, data_item):
@@ -64,47 +63,78 @@ class Preproc:
         return self.data
 
 
-    def map_quantile(self, data_item):
+    def map_numerical_to_categories(self, data_item):
         quantile = data_item.describe()
         data_item.fillna('No data', inplace=True)
 
         for idx, data in data_item.iteritems():
-            pdb.set_trace()
-            if data <= quantile['25%']:
+            if data == 'No data':
+                continue
+
+            elif data < quantile['25%']:
                 data_item.loc[idx] = 'Low'
 
-            elif data > quantile['25%'] and data <= quantile['50%']:
+            elif data >= quantile['25%'] and data < quantile['50%']:
                 data_item.loc[idx] = 'Medium'
 
-            elif data > quantile['50%'] and data <= quantile['75%']:
+            elif data >= quantile['50%'] and data < quantile['75%']:
                 data_item.loc[idx] = 'High'
 
-            elif data > quantile['75%']:
+            elif data >= quantile['75%']:
                 data_item.loc[idx] = 'Excellent'
 
         return data_item
 
 
-    def encode_numerical_to_categories(self):
-        data = self.data[self.num_to_categories]
-        for attribute, item in data.iteritems():
-            mapped_categories = self.map_quantile(item)
-            self.data[attribute] = mapped_categories
+    def word_count(self, data_item):
+        data_item.fillna('', inplace=True)
+
+        for idx, data in data_item.iteritems():
+            filtered_data = re.sub('[^a-zA-Z\s]+', '', data)
+            filtered_data = re.sub(' +', ' ',filtered_data)
+
+            words = filtered_data.split(' ')
+            data_item.loc[idx] = len(words)
+
+        return data_item
+
+    def word_count_amenities(self, data_item):
+        data_item.fillna('', inplace=True)
+        for idx, data in data_item.iteritems():
+            filtered_data = re.sub('[{}"]+', '', data)
+
+            words = filtered_data.split(',')
+            data_item.loc[idx] = len(words)
+
+        return data_item
 
 
-    def apply_preproc(self):
+    def log_price(self):
+        self.data['logPrice'] = np.log(self.data['price'])
+
+
+    def apply_general_preproc(self):
         self.drop_another_countries()
         self.drop_columns()
 
         categorical_data = self.data.select_dtypes(include=['object'])
         for attribute, item in categorical_data.iteritems():
-            normalized_item = self.normalize_text(item)
-            self.data[attribute] = self.clean_signs(normalized_item)
-            #self.data[attribute] = self.map_binary_text(normalized_item)
+            self.data[attribute] = self.clean_signs(item)
 
         for attribute in self.obj_to_float:
-            self.data[attribute] = self.map_str_to_float(self.data[attribute])
-        return self.data
+            data = self.data[attribute].copy()
+            self.data[attribute] = self.map_str_to_float(data)
+
+        for attribute in self.num_to_categories:
+            data = self.data[attribute].copy()
+            self.data[attribute] = self.map_numerical_to_categories(data)
+
+        for attribute in self.text_to_counter:
+            data = self.data[attribute].copy()
+            if attribute == 'amenities':
+                self.data[attribute] = self.word_count_amenities(data)
+            else:
+                self.data[attribute] = self.word_count(data)
 
 
     @staticmethod